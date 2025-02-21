008d6ad2e6c3a6700ddfb59f2b5642fe42900871
Raish0009 <raishmohammad009@gmail.com>
2025-02-21 07:47:52 +0530

    Validation added

--- CreateAccount.c
+++ CreateAccount.c
@@ -1,37 +1,128 @@
 #include <stdio.h>
 
+int choice;
+int num_accounts = 0;
+long long account_numbers[50];
+char account_names[50][50];
+float deposits[50];
+
+void createAccount();
+
+void createAccount()
+{
+
+    printf("\nCreate a bank Account:\n");
+    printf("Enter your account number: ");
+    scanf("%lld", &account_numbers[num_accounts]);
+
+    while (account_numbers[num_accounts] < 11111111111 || account_numbers[num_accounts] > 99999999999)
+    {
+        printf("Please enter an 11-digit number: ");
+        scanf("%lld", &account_numbers[num_accounts]);
+    }
+
+    printf("Enter account holder name: ");
+    scanf("%s", account_names[num_accounts]);
+
+    printf("Enter your deposit amount: ");
+    scanf("%f", &deposits[num_accounts]);
+
+    while (deposits[num_accounts] < 500)
+    {
+        printf("Please deposit a minimum of 500: ");
+        scanf("%f", &deposits[num_accounts]);
+    }
+
+    printf("\nSuccessfully created account:\n\n");
+    printf("Account number: %lld\n", account_numbers[num_accounts]);
+    printf("Account name: %s\n", account_names[num_accounts]);
+    printf("Deposit amount: %.2f\n\n", deposits[num_accounts]);
+
+    num_accounts++;
+}
+
 int main()
 {
-    long long accountNumber;
-    char accountName[50];
-    float initialDeposit;
 
-    printf("Enter your account number: ");
-    scanf("%lld", &accountNumber);
-
-    if (accountNumber < 10000000000 || accountNumber > 99999999999)
+    while (1)
     {
-        printf("Account number must be exactly 11 digits!\n");
-        return;
+
+        printf("======Simple Banking System======\n");
+
+        printf("==============================\n");
+
+        printf("1. Create a bank Account :\n");
+        printf("2. Deposit Money :\n");
+        printf("3. Wihdraw Money :\n");
+        printf("4. Check Balance :\n");
+        printf("5. View Account Details :\n");
+        printf("6. Exits :\n");
+
+        printf("==============================\n");
+
+        printf("Please enter you choice number : ");
+
+        scanf("%d", &choice);
+
+        if (choice == 1)
+        {
+
+            createAccount();
+        }
+
+        else if (choice == 2)
+        {
+            if (num_accounts == 0)
+            {
+                printf("\n ----- No accounts are available please create account first! ----- \n\n");
+            }
+            else
+            {
+                printf(" \n  Deposit Money : \n\n");
+            }
+        }
+        else if (choice == 3)
+        {
+            if (num_accounts == 0)
+            {
+                printf("\n ----- No accounts are available please create account first! ----- \n\n");
+            }
+            else
+            {
+                printf("\n  Withdraw Money\n\n");
+            }
+        }
+        else if (choice == 4)
+        {
+            if (num_accounts == 0)
+            {
+                printf("\n ----- No accounts are available please create account first! ----- \n\n");
+            }
+            else
+            {
+                printf("\n Check your balance :\n\n");
+            }
+        }
+        else if (choice == 5)
+        {
+            if (num_accounts == 0)
+            {
+                printf("\n ----- No accounts are available please create account first! ----- \n\n");
+            }
+            else
+            {
+                printf(" \n View Account  Details :\n\n");
+            }
+        }
+        else if (choice == 6)
+        {
+            printf(" \n  Thank you! for visiting \n\n");
+            break;
+        }
+        else
+        {
+            printf(" \n Invalid Choice Please try again!\n \n");
+        }
     }
-
-    printf("Enter your name: ");
-    scanf("%s", accountName);
-
-    printf("Enter initial deposit: ");
-    scanf("%f", &initialDeposit);
-
-    if (initialDeposit < 500)
-    {
-        printf("Initial deposit must be at least 500!\n");
-        return;
-    }
-
-    printf("Account created successfully!\n");
-
-    printf("Account number: %lld\n", accountNumber);
-    printf("Account name: %s\n", accountName);
-    printf("Initial deposit: %f\n", initialDeposit);
-
     return 0;
 }
